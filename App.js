943aabdf249fd4c7cf94870730a597c3c4f187bf
ZsofiaAntal <antal.zsofia84@gmail.com>
2024-01-09 11:52:26 +0100

    Add dependencies and implement navigation

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 import React, {useState} from 'react';
-import AppLoading from 'expo-app-loading';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import * as Font from 'expo-font';
 import HomeScreen from './screens/HomeScreen';
+import SetUpQuizScreen from './screens/SetUpQuizScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   // const [fontLoaded, setFontLoaded] = useState(false);
@@ -15,8 +19,14 @@ export default function App() {
   //   );
   // }
 
-  return <HomeScreen />
-
+  return (
+  <NavigationContainer>
+  <Stack.Navigator initialRouteName="Home">
+    <Stack.Screen name="Home" component={HomeScreen} />
+    <Stack.Screen name="SetUpQuiz" component={SetUpQuizScreen} />
+  </Stack.Navigator>
+</NavigationContainer>
+  );
 }
 async function loadFonts() {
   await Font.loadAsync({
