b3132d67e61ec627747e169e09b7bb9cdfd9a04c
ZsofiaAntal <antal.zsofia84@gmail.com>
2024-01-10 17:11:11 +0100

    Remove unused assets, screens, and configuration files

--- screens/FriendsScreen.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function FriendsScreen() {
-  return (
-    <Text>FriendsScreen</Text>
-  )
-}
--- screens/LeaderboardScreen.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function LeaderboardScreen() {
-  return (
-    <Text>LeaderboardScreen</Text>
-  )
-}
