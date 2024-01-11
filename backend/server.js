23606a328934d6728b489b07a96aa7f1fd5b913a
ZsofiaAntal <antal.zsofia84@gmail.com>
2024-01-11 12:09:16 +0100

    Add trivia and quiz endpoints to server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-
+//random trivia
 app.get('/surprise', async (req, res) => {
     try {
         const response = await fetch('https://opentdb.com/api.php?amount=1&type=multiple');
@@ -19,6 +19,20 @@ app.get('/surprise', async (req, res) => {
         res.status(500).json({ error: 'An error occurred while fetching quiz data.' });
     }
     });
+
+
+//chosen category and difficulty
+app.get('/quiz', async (req, res) => {
+  try{
+    const { amount = 10, category = 9, difficulty = 'easy', type = 'multiple' } = req.query;
+    const response = await fetch(`https://opentdb.com/api.php?amount=${amount}&category=${category}&difficulty=${difficulty}&type=${type}`);
+  const data = await response.json();
+  res.json(data);
+  }catch(err){
+    console.error('Error:', err);
+    res.status(500).json({ error: 'An error occurred while fetching quiz data.' });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
