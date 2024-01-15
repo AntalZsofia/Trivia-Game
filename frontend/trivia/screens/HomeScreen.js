3770d19b374e9ac29b50c912520473dbbb148d0b
ZsofiaAntal <antal.zsofia84@gmail.com>
2024-01-15 14:33:03 +0100

    Add title image to HomeScreen

--- frontend/trivia/screens/HomeScreen.js
+++ frontend/trivia/screens/HomeScreen.js
@@ -2,13 +2,15 @@
 
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { Image } from 'react-native';
+import Title from '../assets/icons/title.jpg';
 
 
 export default function HomeScreen( {navigation}) {
  
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Trivia Game</Text>
+      <Image source={Title} style={{ width: 400, height: 400 }} resizeMode="contain" />
       <TouchableOpacity style={styles.button} onPress={() => console.log("Sign in pressed")}>
         <Text style={styles.buttonText}>Sign In</Text> 
         </TouchableOpacity>
