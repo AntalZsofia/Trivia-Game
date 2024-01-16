d8aa71033174b6fad80e1c60fd95aa5c476f6b2c
ZsofiaAntal <antal.zsofia84@gmail.com>
2024-01-16 11:25:19 +0100

    Add username and email validation, and password hashing

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,13 +1,53 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const UserSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String,
+    username: {
+        type: String,
+        required: [true, 'Please enter a username.'],
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: [true, 'Please enter an email.'],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, 'Please enter a password.'],
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
+    },
     totalPoints: Number,
     Tournaments: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Tournament'
     }]
     });
+
+    // Hash the password before saving it to the database
+    UserSchema.pre('save', async function (next) {
+    const user = this;
+    if (!user.isModified('password')) return next();
+  
+    try {
+      const salt = await bcrypt.genSalt();
+      user.password = await bcrypt.hash(user.password, salt);
+      next();
+    } catch (error) {
+      return next(error);
+    }
+  });
+  
+  // Compare the given password with the hashed password in the database
+  UserSchema.methods.comparePassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+  };
 
 module.exports = mongoose.model('User', UserSchema);
