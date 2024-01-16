847146bf561a78ecde261ca880fc4f2e9ace3535
ZsofiaAntal <antal.zsofia84@gmail.com>
2024-01-16 11:24:45 +0100

    Add bcrypt and jsonwebtoken dependencies, refactor server.js file

--- backend/server.js
+++ backend/server.js
@@ -1,59 +1,33 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const connectDB = require('./db');
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user');
+const triviaRoutes = require('./routes/trivia');
+
 const cors = require('cors');
-require('dotenv').config();
 const app = express();
 const port = 3000;
 
-
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI)
-.then(() => console.log('MongoDB Connected'))
-.catch(err => console.log(err));
-
-//Models
-const User = require('./models/User');
-const Tournament = require('./models/Tournament');
-const Question = require('./models/Question');
-
-
-
+connectDB();
 
 // Middleware
 app.use(cors());
 
 
+//Parse JSON request body
+app.use(express.json());
 
-// Routes
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+//Define authentication routes
+app.use('/auth', authRoutes);
 
-//random trivia
-app.get('/surprise', async (req, res) => {
-    try {
-        const response = await fetch('https://opentdb.com/api.php?amount=10&type=multiple');
-        const data = await response.json();
-        res.json(data);
-    } catch (err) {
-        console.error('Error:', err);
-        res.status(500).json({ error: 'An error occurred while fetching quiz data.' });
-    }
-    });
+// Define user routes
+app.use('/user', userRoutes);
+
+//Define trivia routes
+app.use('/trivia', triviaRoutes);
 
 
-//chosen category and difficulty
-app.get('/quiz', async (req, res) => {
-  try{
-    const { amount = 10, category = 9, difficulty = 'easy', type = 'multiple' } = req.query;
-    const response = await fetch(`https://opentdb.com/api.php?amount=${amount}&category=${category}&difficulty=${difficulty}&type=${type}`);
-  const data = await response.json();
-  res.json(data);
-  }catch(err){
-    console.error('Error:', err);
-    res.status(500).json({ error: 'An error occurred while fetching quiz data.' });
-  }
-});
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
