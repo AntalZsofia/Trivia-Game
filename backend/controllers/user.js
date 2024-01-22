85988ab88be1c76f7458309c62c0875376747fd6
ZsofiaAntal <antal.zsofia84@gmail.com>
2024-01-22 14:46:11 +0100

    Add user controller and update user routes

---
+++ backend/controllers/user.js
@@ -0,0 +1,24 @@
+const User = require('../models/User');
+//const authenticate = require('../middlewares/auth');
+
+const details = async (req, res) => {
+    try{
+        const user = await User.findById(req.user._id);
+        res.json(user);
+    }catch(err){
+        res.status(400).json({error: err.message});
+    }
+}
+
+const update = async (req, res) => {
+    const { totalPoints, gamesPlayed } = req.body;
+    try{
+        const user = await User.findByIdAndUpdate(req.user._id, { totalPoints, gamesPlayed }, { new: true });
+        res.json(user);
+    }catch(err){
+        res.status(400).json({error: err.message});
+    }
+}
+
+
+module.exports = { details, update };
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const authenticate = require('../middlewares/auth');
 const User = require('../models/User');
+const { update } = require('../controllers/user');
+const { details } = require('../controllers/user');
 
 const router = express.Router();
 
-router.get('/profile', authenticate, (req, res) => {
-    res.json({message: `Welcome: ${req.user.username}`});
-}
-);
+router.get('/details', authenticate, details);
+router.put('/update', update);
 
-router.get('/details', authenticate, async (req, res) => {
-    try{
-        const user = await User.findById(req.user._id);
-        res.json(user);
-    }catch(err){
-        res.status(400).json({error: err.message});
-    }
-});   
+// router.get('/details', authenticate, async (req, res) => {
+//     try{
+//         const user = await User.findById(req.user._id);
+//         res.json(user);
+//     }catch(err){
+//         res.status(400).json({error: err.message});
+//     }
+// });   
 
 module.exports = router;
