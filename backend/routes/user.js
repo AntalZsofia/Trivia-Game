62e69c2893446ceb02f1741d51669d5931a65f30
ZsofiaAntal <antal.zsofia84@gmail.com>
2024-01-23 09:29:42 +0100

    Add authentication to update route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -7,15 +7,7 @@ const { details } = require('../controllers/user');
 const router = express.Router();
 
 router.get('/details', authenticate, details);
-router.put('/update', update);
+router.put('/update', authenticate, update);
 
-// router.get('/details', authenticate, async (req, res) => {
-//     try{
-//         const user = await User.findById(req.user._id);
-//         res.json(user);
-//     }catch(err){
-//         res.status(400).json({error: err.message});
-//     }
-// });   
 
 module.exports = router;
