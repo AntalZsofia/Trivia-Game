904bc0d4f117e6160ec054c69c920598fa8e447e
ZsofiaAntal <antal.zsofia84@gmail.com>
2024-01-29 13:03:02 +0100

    Add inviteFriend function to tournament controller

--- backend/controllers/tournament.js
+++ backend/controllers/tournament.js
@@ -44,4 +44,41 @@ const createTournament = async (req, res) => {
 
 }
 
-module.exports = { createTournament };
+//Invite friend to tournament
+const inviteFriend = async (req, res) => {
+    const { tournamentId, friendId } = req.body;
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({ error: 'Invalid token.' });
+    }
+    const token = authHeader.split(' ')[1];
+
+    try {
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+        const userId = decodedToken.userId
+            ;
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({ error: 'User not found.' });
+        }
+        const tournament = await Tournament.findById(tournamentId);
+        if (!tournament) {
+            return res.status(404).json({ error: 'Tournament not found.' });
+        }
+        const friend = await User.findById(friendId);
+        if (!friend) {
+            return res.status(404).json({ error: 'Friend not found.' });
+        }
+        tournament.users.push(friendId);
+        await tournament.save();
+        res.status(201).json(tournament);
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'An error occurred while inviting friend.' });
+    }
+
+}
+
+module.exports = { createTournament, inviteFriend };
