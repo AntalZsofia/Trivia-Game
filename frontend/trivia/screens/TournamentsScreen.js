74e34b2f706ebd03af56209a96e7c080681401b4
ZsofiaAntal <antal.zsofia84@gmail.com>
2024-02-01 10:26:27 +0100

    Update TournamentsScreen UI and functionality

--- frontend/trivia/screens/TournamentsScreen.js
+++ frontend/trivia/screens/TournamentsScreen.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useContext } from 'react';
-import { View, Text, StyleSheet, Pressable, TextInput } from 'react-native';
+import { View, Text, StyleSheet, Pressable, Image, ScrollView } from 'react-native';
 import { AuthContext } from '../context/AuthContext';
+import Play from '../assets/icons/play.png';
 
 
 export default function TournamentsScreen({ route, navigation }) {
@@ -34,7 +35,79 @@ export default function TournamentsScreen({ route, navigation }) {
     fetchTournaments();
   }, []);
 
+  const colors = ['#66FFDA', '#C5FF66', '#FFF066'];
+  const getBackgroundColor = (index) => colors[index % colors.length];
+
   return (
-    <Text>TournamentsScreen</Text>
+    <ScrollView style={styles.container}>
+      {tournaments.map((tournament, index) => (
+        <View key={tournament._id} style={[styles.tournamentContainer, {backgroundColor: getBackgroundColor(index)}]}>
+          <Text style={styles.tournamentName}>{tournament.name}</Text>
+          <Text style={styles.tournamentCategory}>Category: {tournament.category}</Text>
+          <Text style={styles.tournamentDifficulty}>Difficulty: {tournament.difficulty}</Text>
+          <Text style={styles.userCount}>Users: {tournament.users.length}</Text>
+          <View style={{ flexDirection: 'column', justifyContent: 'space-between',  }}>
+          <Pressable style={styles.button} onPress={() => navigation.navigate('QuizScreen', { questions: tournament.questions })}>
+          
+            <Image source={Play} style={{ width: 40, height: 40 }} />
+            <Text style={styles.buttonText}>Play</Text>
+          
+          </Pressable>
+          </View>
+        </View>
+      ))}
+    </ScrollView>
+
   )
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 20,
+    
+  },
+  tournamentContainer: {
+    backgroundColor: '#fff',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10,
+    marginBottom: 20,
+    elevation: 5,
+    width: '90%',
+    alignSelf: 'center'
+  },
+  tournamentName: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  tournamentScore: {
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  tournamentCategory: {
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  tournamentDifficulty: {
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  userCount: {
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  button: {
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 10,
+    alignItems: 'end'
+  },
+  buttonText: {
+    color: 'black',
+    fontSize: 16,
+    alignSelf: 'end',
+  }
+});
