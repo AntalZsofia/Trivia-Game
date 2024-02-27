d599e6b58ec53054c1d120ea39484c108e166d23
ZsofiaAntal <antal.zsofia84@gmail.com>
2024-02-27 09:54:49 +0100

    Add updateAvatar function to user controller and update user routes

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -2,37 +2,56 @@ const User = require('../models/User');
 //const authenticate = require('../middlewares/auth');
 
 const details = async (req, res) => {
-    try{
+    try {
         const user = await User.findById(req.user._id);
         res.json(user);
         console.log(user);
-    }catch(err){
-        res.status(400).json({error: err.message});
+    } catch (err) {
+        res.status(400).json({ error: err.message });
     }
 }
 
 const update = async (req, res) => {
     const { totalPoints, gamesPlayed } = req.body;
-    try{
+    try {
         const user = await User.findByIdAndUpdate(req.user._id, { totalPoints, gamesPlayed }, { new: true });
         res.json(user);
-    }catch(err){
-        res.status(400).json({error: err.message});
+    } catch (err) {
+        res.status(400).json({ error: err.message });
     }
 }
+//update avatar image
+const updateAvatar = async (req, res) => {
+    const { avatar } = req.body;
+    try {
+        const user = await User.findById(req.user._id);
+        if (!user) {
+            return res.status(404).json({ message: "User not found." })
+        }
+        user.avatar = avatar;
+        await user.save();
+        res.status(200).json({ message: "Avatar updated successfully." });
+    }
+    catch (error) {
+        res.status(500).json({ message: 'Error updating avatar.' });
+    }
+
+}
+
+
 
 const getUserById = async (req, res, next, id) => {
-    try{
+    try {
         const user = await User.findById(id);
-        if(!user){
-            return res.status(400).json({error: 'User not found.'});
+        if (!user) {
+            return res.status(400).json({ error: 'User not found.' });
         }
         req.user = user;
         next();
-    }   catch(err){
-        res.status(400).json({error: err.message});
+    } catch (err) {
+        res.status(400).json({ error: err.message });
     }
 
 }
 
-module.exports = { details, update, getUserById };
+module.exports = { details, update, getUserById, updateAvatar };
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,6 +3,7 @@ const authenticate = require('../middlewares/auth');
 const User = require('../models/User');
 const { update } = require('../controllers/user');
 const { details } = require('../controllers/user');
+const { updateAvatar } = require('../controllers/user')
 const { get } = require('mongoose');
 const { getUserById } = require('../controllers/user');
 
@@ -10,6 +11,7 @@ const router = express.Router();
 
 router.get('/details', authenticate, details);
 router.put('/update', authenticate, update);
+router.put('/avatar/update', authenticate, updateAvatar);
 router.get('/:id', getUserById, (req, res) => res.json(req.user));
 
 
